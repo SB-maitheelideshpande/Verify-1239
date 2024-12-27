fdc5d4ed409a3a374d831940c215c88b56e5c580
seeded-enterprise-owner <seeded-enterprise-owner@example.com>
2024-12-27 12:34:33 +0530

    Modified 6 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight-local.com:8443/api/v1/projects/cHJqOjEzMTI/spectral.js?branch_slug=main&token=7634f0e1-ffa7-4130-b4f5-8cc71a26678a";
+export default { extends: ruleset };
